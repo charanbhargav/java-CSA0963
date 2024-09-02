479d29128ccc98513675a6422b40d5395c0dfd39
charanbhargav <162102844+charanbhargav@users.noreply.github.com>
2024-09-02 14:55:38 +0530

    Add files via upload

---
+++ ndbd.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n,i;
+	cout<<"enter a number:"<<endl;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cout<<n<<"*"<<i<<"="<<n*i<<endl;
+	}
+	return 0;
+}
